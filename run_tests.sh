35756513b2306214295f0c3ead233248b30bdf35
Elias Kunnas <elias.kunnas@gmail.com>
2016-02-29 13:38:58 +0200

    Import of source tarball of version 0.0.11

--- run_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cabal configure -ftest && cabal build && cd tests && ../dist/build/test/test
--- src/Text/LDIF/Consts.hs
+++
@@ -1,6 +0,0 @@
-module Text.LDIF.Consts where
-
--- | Chars necessary to be escaped when are within RDN values
-specialDNChars, escapedDNChars :: [Char]
-specialDNChars = [',','=','+','<','>','#',';']
-escapedDNChars = ['\\', '"'] ++ specialDNChars
